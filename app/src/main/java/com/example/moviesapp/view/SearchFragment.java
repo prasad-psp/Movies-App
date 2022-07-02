a7f06a208d99f30463f304fe5d89011b42fb00d2
Prasad <prasad.parshram123@gmail.com>
2022-07-02 17:38:53 +0530

    added view binding in SearchFragment.java

--- app/src/main/java/com/example/moviesapp/view/SearchFragment.java
+++ app/src/main/java/com/example/moviesapp/view/SearchFragment.java
@@ -9,13 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.databinding.FragmentSearchBinding;
 
 public class SearchFragment extends Fragment {
+
+    FragmentSearchBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        binding = FragmentSearchBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
 }
