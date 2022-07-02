5c4fef702c034af9147085605d03088a5c9d5348
Prasad <prasad.parshram123@gmail.com>
2022-07-02 19:25:13 +0530

    added MovieListRepo logic

--- app/src/main/java/com/example/moviesapp/repo/MovieListRepo.java
+++ app/src/main/java/com/example/moviesapp/repo/MovieListRepo.java
@@ -1,5 +1,42 @@
 package com.example.moviesapp.repo;
 
+import com.example.moviesapp.network.ApiService;
+import com.example.moviesapp.network.RetrofitInstance;
+import com.example.moviesapp.network.response.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MovieListRepo {
 
+    private final ApiService apiService;
+
+    public MovieListRepo() {
+        apiService = RetrofitInstance.getInstance().create(ApiService.class);
+    }
+
+    public void requestMovieList(String movieName, MovieRepoCallback callback) {
+
+        Call<MovieResponse> call = apiService.getMovieBySearch("",movieName);
+
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                callback.onSuccess(response);
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                callback.onFailed(t);
+            }
+        });
+
+    }
+
+
+    interface MovieRepoCallback {
+        void onSuccess(Response<MovieResponse> response);
+        void onFailed(Throwable t);
+    }
 }
