f30fb71c1ccba8748c7c4906433fe62f149b3ccb
Prasad <prasad.parshram123@gmail.com>
2022-07-02 17:56:22 +0530

    added Search Screen logic

--- app/src/main/java/com/example/moviesapp/view/SearchFragment.java
+++ app/src/main/java/com/example/moviesapp/view/SearchFragment.java
@@ -2,7 +2,10 @@ package com.example.moviesapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +18,33 @@ public class SearchFragment extends Fragment {
 
     FragmentSearchBinding binding;
 
+    private static final String MOVIE_KEY = "movie_key";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater,container,false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String movieName = binding.edtSearch.getText().toString().trim();
+
+                if(!movieName.isEmpty()) {
+                    // send movie name into
+                    Bundle bundle = new Bundle();
+                    bundle.putString(MOVIE_KEY,movieName);
+                    Navigation.findNavController(view).navigate(R.id.action_searchFragment_to_movieListFragment,bundle);
+                    binding.edtSearch.setText("");
+                }
+            }
+        });
     }
 }
