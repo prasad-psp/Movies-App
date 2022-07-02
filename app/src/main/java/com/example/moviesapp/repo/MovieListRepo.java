d6a00f243c359fe2116aed1633e3333989534b55
Prasad <prasad.parshram123@gmail.com>
2022-07-02 18:42:54 +0530

    added movie list viewmodel & repo

---
+++ app/src/main/java/com/example/moviesapp/repo/MovieListRepo.java
@@ -0,0 +1,5 @@
+package com.example.moviesapp.repo;
+
+public class MovieListRepo {
+
+}
---
+++ app/src/main/java/com/example/moviesapp/viewmodel/MovieListViewModel.java
@@ -0,0 +1,8 @@
+package com.example.moviesapp.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class MovieListViewModel extends ViewModel {
+
+
+}
