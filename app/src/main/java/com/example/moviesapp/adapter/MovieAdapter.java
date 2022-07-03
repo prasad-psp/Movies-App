2f8feeb069eacc3f596f69aebca6cfffbb50b35a
Prasad <prasad.parshram123@gmail.com>
2022-07-03 12:10:20 +0530

    added logic in MovieAdapter.java

--- app/src/main/java/com/example/moviesapp/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/moviesapp/adapter/MovieAdapter.java
@@ -1,34 +1,95 @@
 package com.example.moviesapp.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.moviesapp.R;
+import com.example.moviesapp.model.Movie;
+
+import java.util.ArrayList;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
+    private final Context context;
+
+    private final ArrayList<Movie> list = new ArrayList<>();
+
+    private OnItemClickListener onItemClickListener;
+
+    public  MovieAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(ArrayList<Movie> movies) {
+        clearData();
+        list.addAll(movies);
+        notifyDataSetChanged();
+    }
+
+    public void clearData() {
+        if(list.size() > 0) {
+            list.clear();
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
 
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_item,parent,false);
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = list.get(position);
 
+        if(movie != null) {
+            Glide.with(context)
+                    .load(movie.getPoster())
+                    .apply(RequestOptions.centerInsideTransform())
+                    .into(holder.imgPoster);
+        }
+
+        holder.rootLayout.setOnClickListener(view -> {
+            if(movie != null) {
+                if(onItemClickListener != null) {
+                    onItemClickListener.onItemClicked(movie);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClicked(Movie movie);
     }
 
     static class MovieViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView imgPoster;
+        private ConstraintLayout rootLayout;
+
         public MovieViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgPoster = itemView.findViewById(R.id.imgMovieItem);
+            rootLayout = itemView.findViewById(R.id.movieListItemRootLayout);
         }
     }
 }
